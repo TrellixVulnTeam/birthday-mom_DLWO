e70e1303702e04768d429f5283804969624cb329
Namish <panderuchi77@gmail.com>
2021-07-29 08:40:45 +0530

    commit 4

--- src/app.js
+++ src/app.js
@@ -8,3 +8,23 @@ button.addEventListener('click', (e)=>{
   })
 })
 
+
+const revealSection = function(entries, observer){
+  const [entry] = entries
+
+  if(!entry.isIntersecting) return
+  entry.target.classList.remove('invisible')
+
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root : null,
+  threshold : .4,
+})
+
+const allSections = document.querySelectorAll('.invisible')
+allSections.forEach(function(section) {
+  sectionObserver.observe(section)
+})
+
